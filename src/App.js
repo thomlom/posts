65d6ef6522b34fe67a57062918e8acedf7497461
Thomas Lombart <t.lombart97@gmail.com>
2019-07-23 14:58:43 +0200

    Add global style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Router } from "@reach/router";
+import { createGlobalStyle } from "styled-components";
 
 import { useAuth } from "./AuthProvider";
 
@@ -8,16 +9,29 @@ import EventDetail from "./EventDetail";
 import Home from "./Home";
 import NotFound from "./NotFound";
 
+const GlobalStyle = createGlobalStyle`
+  @import url('https://rsms.me/inter/inter.css');
+
+  html { font-family: 'Inter', sans-serif; }
+
+  @supports (font-variation-settings: normal) {
+    html { font-family: 'Inter var', sans-serif; }
+  }
+`;
+
 function App() {
   const { isAdmin, isAuthenticated } = useAuth();
 
   return (
-    <Router>
-      <Home path="/" />
-      {isAdmin && <CreateEvent path="/create" />}
-      {isAuthenticated && <EventDetail path="/event/:eventId" />}
-      <NotFound default />
-    </Router>
+    <>
+      <Router>
+        <Home path="/" />
+        {isAdmin && <CreateEvent path="/create" />}
+        {isAuthenticated && <EventDetail path="/event/:eventId" />}
+        <NotFound default />
+      </Router>
+      <GlobalStyle />
+    </>
   );
 }
 
