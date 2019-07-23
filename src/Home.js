08ea12ba7364d9c8155f06d1ea359e90a573f78e
Thomas Lombart <t.lombart97@gmail.com>
2019-07-23 14:52:47 +0200

    Add AuthModal

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
 import { Link } from "@reach/router";
-import { Dialog } from "@reach/dialog";
 
 import { useAuth } from "./AuthProvider";
 
+import AuthModal from "./AuthModal";
 import Events from "./Events";
-import Signin from "./Signin";
 import Signout from "./Signout";
 
 function Home() {
   const [showDialog, setShowDialog] = useState(false);
-  const { isAuthenticated, user, signin } = useAuth();
+  const { isAuthenticated, user } = useAuth();
 
   return (
     <div>
       {isAuthenticated && <p>Hello {user.name}</p>}
       <Link to="/create">Create an event</Link>
       <button onClick={() => setShowDialog(true)}>Show dialog</button>
-      <Dialog isOpen={showDialog} onDismiss={() => setShowDialog(false)}>
-        <Signin signin={signin} />
-      </Dialog>
+      <AuthModal
+        isOpen={showDialog}
+        onDismiss={() => setShowDialog(false)}
+      ></AuthModal>
       <Signout />
       <Events />
     </div>
