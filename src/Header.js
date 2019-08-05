3db6c9216cf5793fdc2c0aaf4812fc16580adc56
Thomas Lombart <t.lombart97@gmail.com>
2019-08-05 14:22:31 +0200

    Add hamburger menu

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { navigate, Link } from "@reach/router";
 
@@ -13,7 +13,7 @@ const NavButton = styled(Button)`
   color: hsl(209, 34%, 30%);
   box-shadow: none;
 
-  @media (max-width: 768px) {
+  @media (max-width: 480px) {
     background-color: hsl(210, 36%, 96%);
   }
 
@@ -23,11 +23,34 @@ const NavButton = styled(Button)`
 `;
 
 const StyledHeader = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
   padding: 0.75rem 1.5rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+
+  @media screen and (min-width: 480px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  div:first-of-type {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  svg {
+    width: 4rem;
+    height: 4rem;
+    cursor: pointer;
+
+    .secondary {
+      fill: hsl(209, 34%, 30%);
+    }
+
+    @media screen and (min-width: 480px) {
+      display: none;
+    }
+  }
 `;
 
 const Title = styled.h1`
@@ -37,42 +60,94 @@ const Title = styled.h1`
   text-decoration: none;
 `;
 
-const Nav = styled.nav`
+const StyledNav = styled.nav`
   button:not(:first-child) {
     margin-left: 1rem;
   }
+
+  @media screen and (max-width: 480px) {
+    display: flex;
+    flex-direction: column;
+
+    button {
+      background-color: white;
+      width: 100%;
+      text-align: left;
+    }
+
+    button:not(:first-child) {
+      margin-top: 1rem;
+      margin-left: 0;
+    }
+  }
 `;
 
-function Header() {
+function Nav() {
   const { isAuthenticated, isAdmin, signout } = useAuth();
-
   const { openDialog } = useDialog();
 
   return (
-    <StyledHeader>
-      <Link to="/" style={{ textDecoration: "none" }}>
-        <Title>Events</Title>
-      </Link>
+    <StyledNav>
+      {isAdmin && (
+        <Link to="/create">
+          <NavButton>Add new event</NavButton>
+        </Link>
+      )}
+      {isAuthenticated ? (
+        <NavButton
+          onClick={() => {
+            signout();
+            navigate("/");
+          }}
+        >
+          Sign Out
+        </NavButton>
+      ) : (
+        <NavButton onClick={openDialog}>Sign In</NavButton>
+      )}
+    </StyledNav>
+  );
+}
 
-      <Nav>
-        {isAdmin && (
-          <Link to="/create">
-            <NavButton>Add new event</NavButton>
-          </Link>
-        )}
-        {isAuthenticated ? (
-          <NavButton
-            onClick={() => {
-              signout();
-              navigate("/");
-            }}
+function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+  const toggleMenu = () => setIsMenuOpen(isOpen => !isOpen);
+
+  return (
+    <StyledHeader>
+      <div>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <Title>Events</Title>
+        </Link>
+        {isMenuOpen ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            class="icon-close"
+            onClick={toggleMenu}
           >
-            Sign Out
-          </NavButton>
+            <path
+              class="secondary"
+              fill-rule="evenodd"
+              d="M15.78 14.36a1 1 0 0 1-1.42 1.42l-2.82-2.83-2.83 2.83a1 1 0 1 1-1.42-1.42l2.83-2.82L7.3 8.7a1 1 0 0 1 1.42-1.42l2.83 2.83 2.82-2.83a1 1 0 0 1 1.42 1.42l-2.83 2.83 2.83 2.82z"
+            />
+          </svg>
         ) : (
-          <NavButton onClick={openDialog}>Sign In</NavButton>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            class="icon-menu"
+            onClick={toggleMenu}
+          >
+            <path
+              class="secondary"
+              fill-rule="evenodd"
+              d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+            />
+          </svg>
         )}
-      </Nav>
+      </div>
+      {isMenuOpen && <Nav />}
     </StyledHeader>
   );
 }
