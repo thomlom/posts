76ff75b0b9c63c8b473a9981c200e60f6dbb9c93
Thomas Lombart <t.lombart97@gmail.com>
2019-08-27 15:10:04 +0200

    Shorten callApi

--- src/services/callApi.js
+++ src/services/callApi.js
@@ -5,14 +5,9 @@ export const TOKEN_NAME = "events_token";
 export default function callApi(endpoint, { ...rest }) {
   const token = window.localStorage.getItem(TOKEN_NAME);
 
-  const headers = {};
-  if (token) {
-    headers.Authorization = `Bearer ${token}`;
-  }
-
   return axios({
     url: `${process.env.REACT_APP_API_URL}${endpoint}`,
-    headers,
+    ...(token ? { headers: { Authorization: `Bearer ${token}` } } : {}),
     ...rest,
   });
 }
