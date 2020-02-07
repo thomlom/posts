2b8ff29457a0c93d5f6513274e65f7c5e784b24b
Thomas Lombart <t.lombart97@gmail.com>
2020-02-07 12:59:42 +0100

    test: add auth test

---
+++ cypress/integration/auth.js
@@ -0,0 +1,34 @@
+import { build, fake } from "@jackfranklin/test-data-bot";
+import { TOKEN_NAME } from "../../src/services/callApi";
+
+const userBuilder = build("User", {
+  fields: {
+    email: fake(f => f.internet.email()),
+    password: fake(f => f.internet.password()),
+    name: fake(f => f.name.firstName()),
+  },
+});
+
+it("signs up the user", () => {
+  const user = userBuilder();
+  cy.server()
+    .route("/post/all", { data: [] })
+    .route("POST", "/signup", { token: "my token" })
+    .route("/me", { user });
+
+  cy.visit("/")
+    .findByText(/sign up/i)
+    .click()
+    .findByLabelText(/email/i)
+    .type(user.email)
+    .findByLabelText(/name/i)
+    .type(user.name)
+    .findByLabelText(/password/i)
+    .type(user.password)
+    .get("button[type='submit']")
+    .click()
+    .window()
+    .its(`localStorage.${TOKEN_NAME}`)
+    .should("eq", "my token")
+    .findByText(/sign out/i);
+});
