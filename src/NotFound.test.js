2bc2382e0cc14c069c2a221391a9e10696802028
Thomas Lombart <t.lombart97@gmail.com>
2020-02-07 13:15:46 +0100

    test: update not found test

--- src/NotFound.test.js
+++ src/NotFound.test.js
@@ -1,19 +1,14 @@
 import React from "react";
-import { screen, render, fireEvent } from "@testing-library/react";
-import { createMemoryHistory } from "history";
-import { Router } from "react-router-dom";
+import { screen, fireEvent } from "@testing-library/react";
+
+import { renderWithRouter } from "./testUtils";
 
 import NotFound from "./NotFound";
 
 test("stumble upon a not found page if the route is not found", () => {
-  const historyMock = createMemoryHistory();
-  render(
-    <Router history={historyMock}>
-      <NotFound />
-    </Router>
-  );
+  const { history } = renderWithRouter(<NotFound />);
 
   screen.getByText(/oops/i);
   fireEvent.click(screen.getByText(/go back/i));
-  expect(historyMock.entries[0].pathname).toBe("/");
+  expect(history.entries[0].pathname).toBe("/");
 });
